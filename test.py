4629e295b11450636228042b25b0dc57ae4bc3d8
unknown <ldandoy@overconsulting.net>
2021-09-13 14:39:25 +0200

    MAJ

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 
+# Connection
 # CONNECTION_STRING="mongodb+srv://epita:epita@cluster0.28nix.mongodb.net/test?retryWrites=true&w=majority"
-CONNECTION_STRING=""
-
+CONNECTION_STRING="mongodb://127.0.0.1/retryWrites=true&w=majority"
 client = MongoClient(CONNECTION_STRING)
 
-dbname = client['test']
+# Data base selection
+dbname = client['epita-test']
 
-user = {
-    "_id": "UIT",
-    "toto": "test"
-}
-
+# Collection selection
 collection_name = dbname["users"]
 
-collection_name.insert_many([user])
+# Insert the new user
+collection_name.insert_one({
+    "_id": ObjectId(),
+    "toto": "test"
+})
+
+# Get all the users from the users collection
+users = collection_name.find()
+for user in users:
+        print(user)
